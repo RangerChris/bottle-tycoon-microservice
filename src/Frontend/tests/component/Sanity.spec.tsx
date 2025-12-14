80ea8c7bb632a9f76cb76c4476f9c73962905a1a
Kresten Elleby <elleby@kresten.dk>
2025-12-14 23:11:06 +0100

    Deleted some more :)

--- src/Frontend/tests/component/Sanity.spec.tsx
+++
@@ -1,7 +0,0 @@
-﻿import React from 'react';
-import { test, expect } from '@playwright/experimental-ct-react';
-
-test('sanity mount inline jsx', async ({ mount }) => {
-  const component = await mount(<div>hello</div>);
-  await expect(component.locator('text=hello')).toBeVisible();
-});
