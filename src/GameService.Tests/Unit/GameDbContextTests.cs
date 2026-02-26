073b8bcb97470fda6a49c5b95a16abab34c44d73
Kresten Elleby <elleby@kresten.dk>
2026-02-26 23:38:20 +0100

    Improve coverage, part 2

---
+++ src/GameService.Tests/Unit/GameDbContextTests.cs
@@ -0,0 +1,118 @@
+﻿using GameService.Data;
+using Microsoft.EntityFrameworkCore;
+using Shouldly;
+using Xunit;
+
+namespace GameService.Tests.Unit;
+
+public class GameDbContextTests
+{
+    [Fact]
+    public void ApplyLowercaseNamingConvention_ConvertsTableNameToLowercase()
+    {
+        var options = new DbContextOptionsBuilder<GameDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDb_TableName")
+            .Options;
+
+        using var context = new GameDbContext(options);
+        var model = context.Model;
+        var playerEntity = model.FindEntityType(typeof(Models.Player));
+
+        playerEntity.ShouldNotBeNull();
+        playerEntity.GetTableName().ShouldBe("players");
+    }
+
+    [Fact]
+    public void ApplyLowercaseNamingConvention_ConvertsColumnNamesToLowercase()
+    {
+        var options = new DbContextOptionsBuilder<GameDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDb_Columns")
+            .Options;
+
+        using var context = new GameDbContext(options);
+        var model = context.Model;
+        var playerEntity = model.FindEntityType(typeof(Models.Player));
+
+        playerEntity.ShouldNotBeNull();
+        var idProperty = playerEntity.FindProperty("Id");
+        idProperty.ShouldNotBeNull();
+        idProperty.GetColumnName().ShouldBe("id");
+
+        var creditsProperty = playerEntity.FindProperty("Credits");
+        creditsProperty.ShouldNotBeNull();
+        creditsProperty.GetColumnName().ShouldBe("credits");
+
+        var nameProperty = playerEntity.FindProperty("Name");
+        nameProperty.ShouldNotBeNull();
+        nameProperty.GetColumnName().ShouldBe("name");
+    }
+
+    [Fact]
+    public void ApplyLowercaseNamingConvention_ConvertsPrimaryKeyNameToLowercase()
+    {
+        var options = new DbContextOptionsBuilder<GameDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDb_PrimaryKey")
+            .Options;
+
+        using var context = new GameDbContext(options);
+        var model = context.Model;
+        var playerEntity = model.FindEntityType(typeof(Models.Player));
+
+        playerEntity.ShouldNotBeNull();
+        var primaryKey = playerEntity.FindPrimaryKey();
+        primaryKey.ShouldNotBeNull();
+    }
+
+    [Fact]
+    public void OnModelCreating_ConfiguresPlayerEntity()
+    {
+        var options = new DbContextOptionsBuilder<GameDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDb_PlayerEntity")
+            .Options;
+
+        using var context = new GameDbContext(options);
+        var model = context.Model;
+        var playerEntity = model.FindEntityType(typeof(Models.Player));
+
+        playerEntity.ShouldNotBeNull();
+        playerEntity.GetTableName().ShouldBe("players");
+
+        var creditsProperty = playerEntity.FindProperty("Credits");
+        creditsProperty.ShouldNotBeNull();
+        creditsProperty.GetPrecision().ShouldBe(18);
+        creditsProperty.GetScale().ShouldBe(2);
+    }
+
+    [Fact]
+    public void DbContext_CanBeConstructed()
+    {
+        var options = new DbContextOptionsBuilder<GameDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDb_Construction")
+            .Options;
+
+        using var context = new GameDbContext(options);
+
+        context.ShouldNotBeNull();
+        context.Players.ShouldNotBeNull();
+    }
+
+    [Fact]
+    public void ApplyLowercaseNamingConvention_HandlesAllProperties()
+    {
+        var options = new DbContextOptionsBuilder<GameDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDb_AllProperties")
+            .Options;
+
+        using var context = new GameDbContext(options);
+        var model = context.Model;
+        var playerEntity = model.FindEntityType(typeof(Models.Player));
+
+        playerEntity.ShouldNotBeNull();
+        foreach (var property in playerEntity.GetProperties())
+        {
+            var columnName = property.GetColumnName();
+            columnName.ShouldNotBeNull();
+            columnName.ShouldBe(columnName.ToLowerInvariant());
+        }
+    }
+}
---
+++ src/GameService.Tests/Unit/GameTelemetryStoreTests.cs
@@ -0,0 +1,117 @@
+﻿using GameService.Services;
+using Shouldly;
+using Xunit;
+
+namespace GameService.Tests.Unit;
+
+public class GameTelemetryStoreTests
+{
+    [Fact]
+    public void SetTotalEarnings_StoresSnapshot()
+    {
+        var store = new GameTelemetryStore();
+        var playerId = Guid.NewGuid();
+        var earnings = 1000m;
+
+        store.SetTotalEarnings(playerId, earnings);
+
+        var snapshots = store.GetAll();
+        snapshots.Count.ShouldBe(1);
+        var snapshot = snapshots.First();
+        snapshot.PlayerId.ShouldBe(playerId);
+        snapshot.TotalEarnings.ShouldBe(earnings);
+    }
+
+    [Fact]
+    public void SetTotalEarnings_WithNegativeValue_StoresZero()
+    {
+        var store = new GameTelemetryStore();
+        var playerId = Guid.NewGuid();
+
+        store.SetTotalEarnings(playerId, -500m);
+
+        var snapshots = store.GetAll();
+        var snapshot = snapshots.First();
+        snapshot.TotalEarnings.ShouldBe(0m);
+    }
+
+    [Fact]
+    public void SetTotalEarnings_UpdatesExistingPlayer()
+    {
+        var store = new GameTelemetryStore();
+        var playerId = Guid.NewGuid();
+
+        store.SetTotalEarnings(playerId, 1000m);
+        store.SetTotalEarnings(playerId, 1500m);
+
+        var snapshots = store.GetAll();
+        snapshots.Count.ShouldBe(1);
+        snapshots.First().TotalEarnings.ShouldBe(1500m);
+    }
+
+    [Fact]
+    public void RemoveAll_ClearsAllSnapshots()
+    {
+        var store = new GameTelemetryStore();
+        store.SetTotalEarnings(Guid.NewGuid(), 1000m);
+        store.SetTotalEarnings(Guid.NewGuid(), 2000m);
+
+        store.RemoveAll();
+
+        var snapshots = store.GetAll();
+        snapshots.Count.ShouldBe(0);
+    }
+
+    [Fact]
+    public void GetAll_ReturnsEmptyCollection_WhenNoSnapshots()
+    {
+        var store = new GameTelemetryStore();
+
+        var snapshots = store.GetAll();
+
+        snapshots.ShouldNotBeNull();
+        snapshots.Count.ShouldBe(0);
+    }
+
+    [Fact]
+    public void GetAll_ReturnsAllSnapshots()
+    {
+        var store = new GameTelemetryStore();
+        var player1 = Guid.NewGuid();
+        var player2 = Guid.NewGuid();
+        var player3 = Guid.NewGuid();
+
+        store.SetTotalEarnings(player1, 1000m);
+        store.SetTotalEarnings(player2, 2000m);
+        store.SetTotalEarnings(player3, 3000m);
+
+        var snapshots = store.GetAll();
+
+        snapshots.Count.ShouldBe(3);
+    }
+
+    [Fact]
+    public void SetTotalEarnings_WithZero_Stores()
+    {
+        var store = new GameTelemetryStore();
+        var playerId = Guid.NewGuid();
+
+        store.SetTotalEarnings(playerId, 0m);
+
+        var snapshots = store.GetAll();
+        snapshots.First().TotalEarnings.ShouldBe(0m);
+    }
+
+    [Fact]
+    public void SetTotalEarnings_WithLargeValue_Stores()
+    {
+        var store = new GameTelemetryStore();
+        var playerId = Guid.NewGuid();
+        var largeValue = 999999999.99m;
+
+        store.SetTotalEarnings(playerId, largeValue);
+
+        var snapshots = store.GetAll();
+        snapshots.First().TotalEarnings.ShouldBe(largeValue);
+    }
+}
